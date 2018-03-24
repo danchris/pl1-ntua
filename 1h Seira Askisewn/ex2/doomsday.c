5de6fcda5b49ec8f77a664ca7971c4dee9998739
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2018-03-24 20:36:57 +0200

    Create list contains (+ -) 1/ex2

--- 1h Seira Askisewn/ex2/doomsday.c
+++ 1h Seira Askisewn/ex2/doomsday.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Node node;
+void insertEnd(node **head, int i, int j, char c);
+void deleteNode(node **head, int i, int j, char c);
+void printList(node *n);
+
+typedef struct Node {
+    int i;
+    int j;
+    char c;
+    struct Node *next;
+}node;
+
 
 int main (int argc, char **argv){
 
@@ -9,11 +21,16 @@ int main (int argc, char **argv){
     int N = 1, M = 1, j = 1;
     char c, map[1002][1002];
 
+    node *head = NULL;
+
     map[1][0] = 'X';
 
     while((c = getc(file)) != EOF){
         if(c!= '\n'){
             map[N][j++] = c;
+            if(c!='.' && c!='X'){
+                insertEnd(&head,N,j,c);
+            }
         }
         else {
             M = j;
@@ -24,18 +41,79 @@ int main (int argc, char **argv){
         }
     }
 
+    fclose(file);
+
     for(int k = 0; k<=M; k++){
         map[0][k] = 'X';
         map[N][k] = 'X';
     }
+
     for(int i = 0; i<=N; i++){
         for(int k = 0; k<=M; k++){
             putchar(map[i][k]);
        }
         putchar('\n');
     }
+    printList(head);
 
 
-    fclose(file);
+
     return 0;
+}
+
+void insertEnd(node **head, int i, int j, char c){
+
+    node *new = (node *)malloc(sizeof(node));
+
+    new->i = i;
+    new->j = j;
+    new->c = c;
+    new->next = NULL;
+
+    if(*head == NULL){
+        *head = new;
+        return;
+    }
+
+    node *last = *head;
+
+    while(last->next != NULL){
+        last = last->next;
+    }
+    last->next = new;
+
+    return;
+}
+
+void deleteNode(node **head, int i, int j, char c){
+
+    node *temp = *head, *prev = NULL;
+
+    if (temp != NULL && (temp->i == i && temp->j == j && temp->c == c)){
+        *head = temp->next;
+        free(temp);
+        return ;
+    }
+
+    while(temp != NULL && (temp->i != i && temp->j != j && temp->c != c)){
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) return ;
+
+    prev->next = temp->next;
+
+    free(temp);
+
+    return ;
+
+}
+
+void printList(node *n){
+
+ while (n != NULL){
+     printf("i = %d, j = %d, c = %c \n", n->i, n->j, n->c);
+     n = n->next;
+  }
 }
