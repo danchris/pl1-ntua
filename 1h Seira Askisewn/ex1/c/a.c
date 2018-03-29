df2f0e8dc0353afc08db777f5ee1969cf5f1dff1
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2018-03-29 18:01:59 +0300

    agora not working in14 in17 overflow

---
+++ 1h Seira Askisewn/ex1/c/a.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main () {
+    printf("%lld\n", ULLONG_MAX);
+    return 0;
+}
--- 1h Seira Askisewn/ex1/c/agora.c
+++ 1h Seira Askisewn/ex1/c/agora.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <inttypes.h>
+#include <math.h>
 
 
-int isPrime (long long int num);
-static long long int findlcm (long long int arr[], int n, int e);
-static long long int gcd (long long int a, long long int b);
+long long int findlcm (long long int a, long long int b);
+long long int gcd (long long int a, long long int b);
+int cmpfunc (const void * a, const void * b);
+int cmpfuncRev (const void * a, const void * b);
 
+int64_t safemult(int64_t a, int64_t b);
 int main (int argc, char **argv){
 
     if (argc < 2) {
@@ -22,72 +27,53 @@ int main (int argc, char **argv){
         exit(1);
     }
 
-    long long int maxVillage = -1, maxPrimeValue = -1, *villages = malloc(N*sizeof(long long int));
-    int i = 0, max = -1, maxPrime = -1;
+    int i = 0;
+    long long int *villages = malloc(N*sizeof(long long int)), *runningLcm = malloc(N*sizeof(long long int)), *runningLcmReverse = malloc(N*sizeof(long long int));
 
-    /*
-     * Read input file
-     * and store
-     * to array
-     * Also store maximum value of array
-     * and maximum prime value of array if exists
-     */
 
-    while((fscanf(file,"%lld", &villages[i])) == 1){
-        if(villages[i] > max){
-            max = villages[i];
-            maxVillage = i;
-        }
-        if(isPrime(villages[i])){
-            if(villages[i] > maxPrimeValue){
-                maxPrime = i;
-                maxPrimeValue = villages[i];
+    long long int temp;
+    while((fscanf(file,"%lli", &villages[i])) == 1) i++;
+    runningLcm[0] = villages[0];
+    runningLcmReverse[N-1] = villages[N-1];
+
+    for(i=1;i<N;i++){
+       runningLcm[i] = temp = findlcm(runningLcm[i-1],villages[i]);
+       runningLcmReverse[N-i-1] = findlcm(runningLcmReverse[N-i],villages[N-i-1]);
+    }
+
+    long long int max = runningLcmReverse[0];
+    long long int min = max;
+    int v = 0;
+
+    for(i=0; i<N;i++){
+        printf("runningLcm[%d] = %lli runningLcmReverse[%d] = %lli\n", i, runningLcm[i], i, runningLcmReverse[i]);
+    }
+    printf("---------------------------------\n");
+
+    for(i=1; i<N-1; i++){
+        printf("runningLcm[%d] = %lli runningLcmReverse[%d] = %lli\n", i-1, runningLcm[i-1], i+1, runningLcmReverse[i+1]);
+        temp = findlcm(runningLcm[i-1],runningLcmReverse[i+1]);
+        if(runningLcm[i-1] < runningLcm[i] && runningLcmReverse[i] > runningLcmReverse[i+1]) {
+            temp = findlcm(runningLcm[i-1],runningLcmReverse[i+1]);
+            printf("temp = %lld\n", temp);
+            if(temp<min && temp>0){
+                printf("prin = %lld meta = %lld\n", min, temp);
+                min = temp;
+                v = i+1;
             }
         }
-        i++;
     }
 
+    printf("%lld %d\n", min, v);
     fclose(file);
-    long long int lcm = findlcm(villages,N,-1), other = -1;     //calculate lcm of array
-    int v = -1, otherP = -1, otherV = -1;
-    /*
-     * If exists prime calculate lcm again with it
-     * or calculate lcm without biggest element
-     * and compare with lcm
-     */
-    if (maxPrime != -1){
-        other = findlcm(villages,N,maxPrime);
-        otherP = maxPrime;
-    }
-    else {
-        other = findlcm(villages,N,maxVillage);
-        otherV = maxVillage;
-    }
-
-
-    if (other!= -1 && other < lcm){
-        lcm = other;
-        v = ( otherP > otherV ? otherP : otherV);
-    }
-    printf("%lld %d\n", lcm, v+1);
     free(villages);
+    free(runningLcm);
+    free(runningLcmReverse);
 
     return 0;
 }
 
-int isPrime(long long int num){
-
-    if (num <= 1) return 0;
-    if (num % 2 == 0 && num > 2) return 0;
-    for (int i = 3; i < num / 2; i+=2){
-        if (num % i == 0){
-            return 0;
-        }
-    }
-    return 1;
-}
-
-static long long int gcd(long long int a, long long int b){
+long long int gcd(long long int a, long long int b){
 
 
     if (b==0) return a;
@@ -95,19 +81,40 @@ static long long int gcd(long long int a, long long int b){
     return gcd(b,a%b);
 }
 
-static long long int findlcm(long long int arr[], int n, int e){
 
-    long long int ans = arr[0];
+long long int findlcm (long long int a, long long int b){
 
-    for (int i = 1; i < n; i++){
-        if (i == e) continue;
-        if( ans > arr[i]) {
-            ans = (ans/gcd(ans,arr[i]))*arr[i];
-        }
-        else ans = (arr[i]/gcd(ans,arr[i]))*ans;
-      //  ans = ( ((arr[i]*ans)) / (gcd(arr[i],ans)) );
-    }
 
-    return ans;
+ //   return safemult(a,b) / gcd(a,b);
+ long long int ans = (a*b)/gcd(a,b);
+ if(ans > 0) return ans;
+ return LLONG_MAX + ans/64;
+  //  return (ans > 0 ) ? ans : (a/64 * b/64)/gcd(a/64,b/64);
+   // return  (a*b)/gcd(a,b) ;
 }
 
+int cmpfunc (const void * a, const void * b){
+    if( *(long long int*)a - *(long long int*)b < 0 )
+        return -1;
+    if( *(long long int*)a - *(long long int*)b > 0 )
+        return 1;
+    return 0;
+}
+
+int cmpfuncRev (const void * a, const void * b){
+    return !cmpfunc(a,b);
+}
+
+int64_t safemult(int64_t a, int64_t b) {
+  double dx;
+
+  dx = (double)a * (double)b;
+
+  if ( fabs(dx) < (double)9007199254740992 )
+    return (int64_t)dx;
+
+  if ( (double)INT64_MAX < fabs(dx) )
+    return INT64_MAX;
+
+  return a*b;
+}
