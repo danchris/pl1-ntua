6a86c1528ca889b6218226fd599e7d2ab9ee6432
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2018-05-07 00:31:39 +0300

    A solution for 2/ex1 java

--- 2h Seira Askisewn/ex1/Pistes.java
+++ 2h Seira Askisewn/ex1/Pistes.java
@@ -101,33 +101,137 @@ class Player {
     public HashMap<Integer,Integer> getKeysOwn(){
         return keysOwn;
     }
-/*
+
+    public int getNumberOwn(){
+        return numberOwn;
+    }
+
+    public void setPista(Pista newPista) {
+        this.currPista = newPista;
+    }
+
+    public void setKeysOwn(HashMap<Integer,Integer> newKeys){
+        this.keysOwn = newKeys;
+    }
+
+    public void setTotalStars(long newStars){
+        this.totalStars = newStars;
+    }
+
+    public void setNumberOwn(int newNumber){
+        this.numberOwn = newNumber;
+    }
+
     public Pista giveMeNextPista(ArrayList<Pista> availablePistes){
 
-        for (Pista it : restPistes) {
-            ArrayList<Integer> itNeeded = it.getNeeded();
-            ArrayList<Integer> common = new ArrayList<Integer>(itNeeded);
-            common.retainAll(this.getNeeded());
-            System.out.println("The common collection is : " + common);
+        Pista ret = null;
+
+      //  Collections.sort(availablePistes);
+        for (Pista it : availablePistes) {
+
+            HashMap<Integer,Integer> myK = this.keysOwn;
+            HashMap<Integer,Integer> tmp = it.getNeeded();
+
+            int flag = 0;
+            for (Map.Entry<Integer,Integer> entry : tmp.entrySet()){
+
+                int key = entry.getKey();
+                int value = entry.getValue();
+
+                if (myK.containsKey(key)) {
+                    int tmpValue = myK.get(key);
+
+                    if (value <= tmpValue) {
+                        flag += value;
+                    }
+                }
+                else break;
+            }
+
+
+            if (flag == it.getNumberOfNeeded()) {
+                ret = it;
+                break;
+            }
+
         }
 
-        return it;
+        return ret;
 
     }
-*/
+
+    public void playPista (Pista toPlay){
+
+        HashMap<Integer,Integer> pK = toPlay.getNeeded();
+        HashMap<Integer,Integer> pE = toPlay.getEarnings();
+        HashMap<Integer,Integer> myK = this.keysOwn;
+        long s = toPlay.getStars();
+        int n = toPlay.getNumberOfNeeded();
+
+
+        for (Map.Entry<Integer,Integer> entry : pK.entrySet()){
+
+            int key = entry.getKey();
+            int value = entry.getValue();
+            int newV = myK.get(key);
+
+            if (newV == value) {
+                myK.remove(key);
+            }
+            else myK.put(key, Math.abs(newV-value));
+        }
+
+        for (Map.Entry<Integer,Integer> entry : pE.entrySet()){
+
+            int key = entry.getKey();
+            int value = entry.getValue();
+
+            if (myK.containsKey(key)){
+                int newV = myK.put(key, myK.get(key) + value);
+            }
+            else myK.put(key,value);
+        }
+
+        this.totalStars += s;
+        this.numberOwn = Math.abs(numberOwn - n);
+        this.currPista = toPlay;
+        this.keysOwn = myK;
+        return ;
+
+    }
+
 }
 
 public class Pistes {
 
+
+    public static void printAllPistes(ArrayList<Pista> allPistes) {
+
+        System.out.println("--------Time to print all pistes ------------");
+        for (Pista p : allPistes) {
+            System.out.print("Id = " + p.getID());
+            System.out.print(" sunolika thelw " + p.getNumberOfNeeded() + " ta opoia einai ");
+            printHashMap(p.getNeeded());
+            System.out.print(" kai sou dinw  " + p.getNumberOfEarnings() + " ta opoia einai " );
+            printHashMap(p.getEarnings());
+            System.out.println(" stars " + p.getStars());
+        }
+
+        System.out.println("--------End to print all pistes ------------");
+    }
     public static void printHashMap(HashMap<Integer,Integer> map){
 
 
-        if (map.isEmpty()) System.out.print("Map is Empty ");
+        if (map.isEmpty()) {
+            System.out.print("Map is Empty ");
+            return ;
+        }
         for (Map.Entry<Integer,Integer> entry : map.entrySet()){
             int key = entry.getKey();
             int value = entry.getValue();
             System.out.print("Key = " + key + " Value =  " + value + " " );
         }
+        System.out.print(" ");
     }
     public static void printArrayList(ArrayList<Integer> l){
         System.out.println("Tha tupwsw ena arraylist me kleidia");
@@ -142,11 +246,19 @@ public class Pistes {
     public static ArrayList<Pista> giveMeAllPistesAvailable(ArrayList<Pista> l, int keyNumber){
 
         ArrayList<Pista> ret = new ArrayList<Pista>();
+
+        if (l.isEmpty()) return null;
+
+        Collections.sort(l);
+
         for (Pista p : l){
             if (p.getNumberOfNeeded() <= keyNumber) {
                 ret.add(p);
             }
+            else break;
         }
+
+        if (ret.isEmpty()) return null;
 
         return ret;
     }
@@ -160,7 +272,6 @@ public class Pistes {
             int counter = 0;
             ArrayList<Pista> allPistes = new ArrayList<Pista>();
 
-            System.out.println(N);
             line = reader.readLine();
             while(line != null && !(line.matches("\\s*"))) {
 
@@ -196,36 +307,31 @@ public class Pistes {
                 line = reader.readLine();
             }
 
-            Collections.sort(allPistes);
-            for (Pista p : allPistes) {
-                System.out.print("Id = " + p.getID());
-                System.out.print(" sunolika thelw " + p.getNumberOfNeeded() + " ta opoia einai ");
-                printHashMap(p.getNeeded());
-                System.out.print(" kai sou dinw  " + p.getNumberOfEarnings() + " ta opoia einai " );
-                printHashMap(p.getEarnings());
-                System.out.println(" stars " + p.getStars());
+            //Collections.sort(allPistes);                    //Gia na exw sthn arxh thn 0
+
+
+            Pista curr = allPistes.get(0);                  //Arxh
+            allPistes.remove(0);                            //Afairw thn arxh
+
+            Player myPlayer = new Player(curr, curr.getEarnings(), curr.getStars(),curr.getNumberOfEarnings());
+
+            ArrayList<Pista> tmpAvailable = null;
+
+            while( ( ! myPlayer.getKeysOwn().isEmpty()) && (! allPistes.isEmpty())) {
+
+                tmpAvailable = giveMeAllPistesAvailable(allPistes, myPlayer.getKeysOwn().size());
+
+                if (tmpAvailable == null) break;            // An dn mporw na paiksw se kamia pista logo oti dn exw ariithmo kleidiwn
+                curr = myPlayer.giveMeNextPista(tmpAvailable);
+                if (curr == null) break;                    // An dn exw ton swsto sundiasmo kleidiwn
+
+                myPlayer.playPista(curr);                   //paizw thn pista
+
+                allPistes.remove(curr);                     //Afairw apo tin lista authn pou molis epaiksa
             }
 
-        //    Collections.sort(allPistes);
+            System.out.println(myPlayer.getTotalStars());
 
-/*
-            Pista start = allPistes.get(0);
-            allPistes.remove(0);
-            ArrayList<Integer> startKeys = start.getEarnings();
-            long startStars = start.getStars();
-
-            Player myPlayer = new Player(start, startKeys, startStars);
-
-            System.out.println("Eimai o player kai ksekinaw apo thn Pista " + start.getID() + " kai molis kerdisa " + startKeys.size() + " kai sunolika asteria exw " + startStars);
-
-            printArrayList(startKeys);
-
-            ArrayList<Pista> tmpAvailable = allPistes;
-            while( ! myPlayer.getKeysOwn().isEmpty() ) {
-                tmpAvailable = giveMeAllPistesAvailable(tmpAvailable, myPlayer.getKeysOwn().size());
-                Collections.sort(tmpAvailable);
-            }
-*/
         }
         catch(IOException e){
             e.printStackTrace();
