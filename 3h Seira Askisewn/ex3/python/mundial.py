4f623348b2eae24e554ff05fe8f924750d0736a8
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2018-05-25 17:38:05 +0300

    Try to build skeleton 3/ex3/python

--- 3h Seira Askisewn/ex3/python/mundial.py
+++ 3h Seira Askisewn/ex3/python/mundial.py
@@ -2,6 +2,30 @@
 
 import sys
 import os
+import heapq as hq
+
+
+def Solver(state):
+    curr = state[0]
+    List = state[1]
+
+    currM = curr[0]
+    currG = curr[1]
+    currF = curr[2]
+
+    print("Eimaii h omada", curr[3], "kai tha paiksw me")
+    for it in List:
+        print(" Team ", it[3], end='')
+        tmpM = it[0]
+        tmpG = it[1]
+        tmpF = it[2]
+
+        if (currG >= tmpF):
+            print("Mporw na to paiksw")
+        else:
+            print(" Den mporw giati", currG, " kai ", tmpG )
+
+
 
 def printList(myList):
     for i in myList:
@@ -14,7 +38,8 @@ def main(argv):
     file = open(sys.argv[1],"r")
     last = tuple()
     N = 1
-    myList = list()
+    myList = []
+    tmpTuple = tuple()
 
     with file as f:
         N = int(f.readline())
@@ -23,12 +48,25 @@ def main(argv):
             if not line: break
             line = line.replace("\r", "").replace("\n", "")
             last = line.split(" ")
-            myList.append(last)
+            tmpTuple = (last[1],last[2],last[3],last[0])
+            myList.append(tmpTuple)
 
         f.close()
 
     print(N)
+    ## convert list to heapq
+    ## list contains tuples of (matches,goals,fails,team)
+    hq.heapify(myList)
     printList(myList)
+
+
+    openSet = list()
+    openSet.append((hq.heappop(myList),myList))
+
+    while len(openSet) > 0:
+        tmpState = openSet.pop()
+        Solver(tmpState)
+
 
 
 if __name__ =="__main__":
