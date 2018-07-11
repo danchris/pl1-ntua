1ae32c4e3d009f06b9b16868504e2fbf25608c16
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2018-07-12 00:52:46 +0300

    Reader for mundial prolog added

---
+++ 3h Seira Askisewn/ex3/prolog/mundial.pl
@@ -0,0 +1,23 @@
+mundial(File, Answer):-
+    read_input(File, _, Answer).
+
+
+read_input(File, N, Teams) :-
+    open(File, read, Stream),
+    read_line_to_codes(Stream, Line),
+    atom_codes(Atom, Line),
+    atom_number(Atom, N),
+    read_lines(Stream, N, Teams).
+
+read_lines(Stream, N, Teams) :-
+    ( N == 0 -> Teams = []
+    ; N > 0  -> read_line(Stream, Team),
+                Nm1 is N-1,
+                read_lines(Stream, Nm1, RestTeams),
+                Teams = [Team | RestTeams]).
+
+read_line(Stream, team(Name, P, A, B)) :-
+    read_line_to_codes(Stream, Line),
+    atom_codes(Atom, Line),
+    atomic_list_concat([Name | Atoms], ' ', Atom),
+    maplist(atom_number, Atoms, [P, A, B]).
